c14b3206b52d440f19f33991d0274fc9d582309f
REXgamer13 <gelvezbotiajuansebastian@gmail.com>
2026-04-18 15:52:13 -0500

    Initial commit, creacion del proyecto y ejecucion exitosa.

---
+++ src/main/java/com/universidad/patrones/GelvezPost2U5Application.java
@@ -0,0 +1,13 @@
+package com.universidad.patrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GelvezPost2U5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GelvezPost2U5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/universidad/patrones/bibliotecaapi/GelvezPost2U5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidad.patrones.bibliotecaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GelvezPost2U5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
